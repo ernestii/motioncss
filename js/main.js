73c27524071c2f921801bbf7c3424e052ff6c868
Ernest Iliiasov <me@ernest.asia>
2018-03-03 23:09:02 -0500

    Build HTML page, add elements, add css and js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// After DOM is ready
+$(document).ready(function() {
+
+  const $sw = $('#switch');
+
+  // When the switch is clicked
+  $sw.on('click', function() {
+    $sw.toggleClass('active');
+  });
+
+});
