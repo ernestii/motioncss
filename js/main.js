99facecad123cfa99632e7ab0bb8ea536a71ca06
Ernest Iliiasov <me@ernest.asia>
2018-03-04 09:18:13 -0500

    Add Clouds

--- js/main.js
+++ js/main.js
@@ -7,6 +7,46 @@ $(document).ready(function() {
   $sw.on('click', function() {
     $sw.toggleClass('active');
     $('body').toggleClass('night');
+
+    if ( ! $('body').hasClass('night') ) {
+
+      // Clouds animation
+      let i = 0;
+      $('.cloud').each(function() {
+        const $this = $(this);
+
+        /* Add delay for each cloud using setTimeout */
+        setTimeout(function() {
+          $this.animate({
+            opacity: .6,
+            bottom: '1vh',
+          }, { 
+            duration: 2000,
+            easing: 'easeOutQuart'
+          });
+        }, i * 200);
+        
+        i++;
+      });
+    } else {
+      
+      // Clouds animation
+      let i = 0;
+      $('.cloud').each(function() {
+          $(this).animate({
+            opacity: 0,
+            bottom: '-100%',
+          }, { 
+            duration: 1000 + i * 100,
+            easing: 'easeInOutQuart'
+          });
+        i++;
+      });
+
+    }
+    
   });
+
+  
 
 });
